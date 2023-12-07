dc9f6b020d22bb2bcda76fc5eef4f40007b4d887
Predrag Damjanović <Predrag.Damjanovic@prointer.ba>
2023-12-07 15:13:08 +0100

    Added Siralog

--- TravelOrder.Server/Controllers/WeatherForecastController.cs
+++ TravelOrder.Server/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace TravelOrder.Server.Controllers
 {
@@ -21,13 +22,15 @@ namespace TravelOrder.Server.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var temp = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+            Log.Information("Wather Forecasts => {@temp}", temp);
+            return temp;
         }
     }
 }
--- TravelOrder.Server/Program.cs
+++ TravelOrder.Server/Program.cs
@@ -1,4 +1,7 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Serilog configuration
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration).CreateLogger();
 
 var app = builder.Build();
 
